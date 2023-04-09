dac96d59057b11e0d9ae2ad0c838fdfa270c6c32
Juan Pablo Casarino <juan21casarino@gmail.com>
2023-04-09 19:53:27 -0300

    Primera correcion

--- app.js
+++ app.js
@@ -7,32 +7,36 @@ const port = 8080;
 const productManager = new ProductManager('products.json');
 
 app.get('/products', async (req, res) => {
-    try {
-      const limit = parseInt(req.query.limit);
-      const products = await productManager.getProducts(limit);
-      res.json(products);
-    } catch (error) {
-      console.error(error);
-    }
-  });
+  try {
+    const limit = req.query.limit; // Obtenemos el query param limit (si existe)
+    const products = await productManager.getProducts(); 
 
-  app.get('/products/:id', async (req, res) => {
-    try {
-      const id = parseInt(req.params.id);
-      const product = await productManager.getProductById(id);
-
-      if (!product) {
-        return console.log('Product not found');
-      }
-      else{
-        res.json(product);
-      }
-    } 
-    catch (error) {
-      console.error(error);
+    if (limit) {
+      res.json(products.slice(0, limit)); // Si existe limit, devolvemos sólo los primeros 'limit' productos
+    } else {
+      res.json(products); // Si no existe limit, devolvemos todos los productos
     }
-  });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const product = await productManager.getProductById(id);
+
+    if (!product) {
+      return console.log('Product not found');
+    } else {
+      res.json(product);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Servidor Express escuchando en el puerto ` + port);
-  });
+  console.log(`Servidor Express escuchando en el puerto ` + port);
+});
